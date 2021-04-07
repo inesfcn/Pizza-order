b067d12299f756f095f7eb235adf1cb2e108c8a1
INes <inesfcnogueira@gmail.com>
2021-04-07 16:54:27 +0200

    added cart on orders

--- client/src/components/Order.jsx
+++ client/src/components/Order.jsx
@@ -1,8 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Margherita from "../assets/pizza.png";
-import Buffalina from "../assets/pizza(1).png";
-import Bianca from "../assets/pizza(3).png";
-import salsicia from "../assets/pizza(4).png";
 import { getPizzas } from "../helpers/apiCalls"
 
 const Order = () =>{
@@ -17,16 +13,54 @@ const Order = () =>{
         get();
     },[])
 
-    const pizzaList = pizzas.map((pizza, idx)=>{
+    const increment = (e) =>{
+        setPizzas(pizzas.map((pizza)=>{
+         if(pizza.name===e.target.name && pizza.quantity!== undefined){
+           return {...pizza, quantity:(pizza.quantity+=1)}
+         }else if(pizza.name===e.target.name && pizza.quantity=== undefined){
+           return {...pizza, quantity:1}
+         }else{
+           return pizza
+         }
+        }))
+    }
+    
+    const decrement = e =>{
+        setPizzas(pizzas.map((pizza)=>{
+            if(pizza.name===e.target.name && pizza.quantity>0){
+                return {...pizza, quantity:(pizza.quantity-=1)}
+              }else{
+                  return pizza
+              }
+        }))
+    }
+
+
+    const pizzaList = pizzas.map((pizza)=>{
         return(
             <div className="pizza-container" key={pizza._id}>
-                <img src={pizza.name} alt={pizza.name}/>
-                <h2>{pizza.name}</h2>
-                <h5>{pizza.price}</h5>
-                <button name="tomato">Add</button>
+                <img src={pizza.img} alt="pizza icon"/>
+                <div className="desc-container">
+                    <h2>{pizza.name}</h2>
+                    <h5>{pizza.price}</h5>
+               </div>
+                <button name="tomato" name={pizza.name} onClick={increment}>Add</button>
             </div>
         )
     })
+
+    const pizzasAdded = pizzas.filter((pizza)=> pizza.quantity)
+
+
+    const pizzasAddedList = pizzasAdded.map((pizza)=>{
+        return(
+            <div className="pizza-added-container" key={pizza._id}>
+                <p><b>{pizza.name}</b> | {pizza.quantity} <button onClick={increment} name={pizza.name}>+</button><button name={pizza.name} onClick={decrement}>-</button></p>
+            </div>
+        )
+    })
+    console.log(pizzas);
+    console.log(pizzasAdded);
 
     return(
         <div className="order">
@@ -34,9 +68,16 @@ const Order = () =>{
                 Order our amazing Pizzas
             </h1>
             <h3>Every Pizza can be made with vegan cheese</h3>
-            <div className="order-images">
-                {pizzaList}
-            </div>
+          <div className="orders-container">
+                <div className="pizzas-container">
+                    {pizzaList}
+                </div>
+                {pizzasAdded.length>0 &&
+                <div className="modal-ordered">
+                     {pizzasAddedList}
+                     <button className="order-btn">Order</button>
+                </div>}
+          </div>
         </div>
     )
 }
